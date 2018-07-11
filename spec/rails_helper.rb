b7bb6f7b181ab0941858c3c463c7f0fee42e45eb
caiodsc <caio.dscamara@gmail.com>
2018-07-11 14:03:01 -0400

    Forms controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |file| require file }
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -7,6 +6,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |file| require file }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
